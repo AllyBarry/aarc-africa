3e20bb86e802c687ad346ac12250418f7489ab9c
Alexandra <ali.barry3@gmail.com>
2026-03-18 01:11:15 +0200

    update dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://aarc.africa',
+  site: 'https://aarc-africa.site',
   vite: {
     plugins: [tailwindcss()],
   },
